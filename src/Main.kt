d64ef8f931520791c709d77c7e5e6524d181a6df
A19AlbertoNS <a19albertons@iessanclemente.net>
2024-11-04 09:48:55 +0100

    añadimos normas basicas y mecanismo basico para comprobación

--- src/Main.kt
+++ src/Main.kt
@@ -6,7 +6,23 @@ fun jugar(){
     val numero_random= Random.nextInt(1000,9999)
     val numero_random_split=numero_random.toString().toList()
     val array_numero_random=numero_random_split.toTypedArray()
+    println(array_numero_random[0])
+    println("los numero van del 1000 al 9999")
+    println("Si te sale asterisco es que el numero no es coincidente")
+    println("teclea un numero de 4 cifras")
+    val numero= readln()
+    val numero_lista=numero.toList()
 
+    for (i in 0 until array_numero_random.size) {
+//        println(array_numero_random[i])
+//        println(numero_lista[i])
+        if (array_numero_random[i].toString().toInt()==numero_lista[i].toString().toInt()) {
+            print(array_numero_random[i])
+        }
+        else {
+            print("*")
+        }
+    }
 }
 fun trazas() {
 
