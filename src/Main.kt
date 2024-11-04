3208266cf2088f3c0fe1739abe4a70a2a596589b
A19AlbertoNS <a19albertons@iessanclemente.net>
2024-11-04 09:36:04 +0100

    menu basico y genereacion numero random

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,33 @@
+import kotlin.random.Random
+import kotlin.system.exitProcess
+
+fun jugar(){
+    //    Crear un numero aleatorio de 4 cifras según ChatGPT
+    val numero_random= Random.nextInt(1000,9999)
+    val numero_random_split=numero_random.toString().toList()
+    val array_numero_random=numero_random_split.toTypedArray()
+
+}
+fun trazas() {
+
+}
+fun salir() {
+    exitProcess(0)
+}
+
 fun main() {
+    println("1. Jugar")
+    println("2. Ver traza del ultimo intento")
+    println("3. Salir")
+    var eleccion= readln().toInt()
+    while (eleccion!=3) {
+        when (eleccion) {
+            1 -> jugar()
+            2 -> trazas()
+            3 -> salir()
+            else -> println("ha introducido un numero erroneo no se hace nada")
+        }
+        eleccion= readln().toInt()
+    }
 
 }
