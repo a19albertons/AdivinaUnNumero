245f75fd4ed1f99439f8b200900201757f5feeff
A19AlbertoNS <a19albertons@iessanclemente.net>
2024-11-04 10:38:47 +0100

    Añadimos el log al git ignore, se completa la creacion de fichero

--- src/Main.kt
+++ src/Main.kt
@@ -9,7 +9,7 @@ fun jugar(){
     val numero_random= Random.nextInt(1000,9999)
     val numero_random_split=numero_random.toString().toList()
     val array_numero_random=numero_random_split.toTypedArray()
-    println(array_numero_random[0])
+//    println(array_numero_random[0])
     println("los numero van del 1000 al 9999")
     println("Si te sale asterisco es que el numero no es coincidente")
     println("Si repites algun numero puede dar falsos duplicados en las posiciones no coincidentes")
@@ -18,21 +18,23 @@ fun jugar(){
     var numero= readln()
     val intentos=10
     var num_intentos=0
+    File(fichero).writeText("El numero secreto era: "+numero_random+"\n")
+
     while (num_intentos<intentos) {
         num_intentos++
-        File(fichero).writeText("El numero secreto era: "+numero_random+"\n")
         val numero_lista=numero.toList()
         val numeros_no_coincidete_random= mutableListOf<Int>()
         val numeros_no_coincidete_dado= mutableListOf<Int>()
 
         File(fichero).appendText("Intento $num_intentos: " + numero)
+        var contador_acierto=0
 //        Gestiona la comprobación del numero 100% coincidente
         for (i in 0 until array_numero_random.size) {
 //        println(array_numero_random[i])
 //        println(numero_lista[i])
             if (array_numero_random[i].toString().toInt()==numero_lista[i].toString().toInt()) {
                 print(array_numero_random[i])
-
+                contador_acierto++
             }
             else {
                 print("*")
@@ -42,15 +44,25 @@ fun jugar(){
                 numeros_no_coincidete_dado.add(numero_lista[i].toString().toInt())
             }
         }
+        File(fichero).appendText(" Aciertos: $contador_acierto,")
+        var contador_coindicendia=0
         if (numeros_no_coincidete_dado.size>0) {
-            print(" Estos son los numeros coincidentes en otra posicion1 si hay:")
+            print(" Estos son los numeros coincidentes en otra posicion si hay:")
             for (i in numeros_no_coincidete_dado) {
                 if (i in numeros_no_coincidete_random) {
                     print(" "+i)
+                    contador_coindicendia++
                 }
             }
         }
+        File(fichero).appendText(" Coincidencias: $contador_coindicendia\n")
         println()
+        println(numero)
+        println(numero_random)
+        if (numero.toInt()==numero_random.toString().toInt()) {
+            println("Gracias por jugar")
+            break
+        }
         numero= readln()
 
     }
@@ -74,6 +86,11 @@ fun main() {
             2 -> ver_trazas()
             else -> println("ha introducido un numero erroneo no se hace nada")
         }
+
+        println()
+        println("1. Jugar")
+        println("2. Ver traza del ultimo intento")
+        println("3. Salir")
         eleccion= readln().toInt()
     }
 
